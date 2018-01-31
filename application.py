9b3a9340bed2f9c2fe7b36bae35697d491215601
Robert van Veelen <robert.vanveelen@gmail.com>
2018-01-31 07:24:38 -0600

    getenv

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    ver = os.env.get('VERSION', 'undef')
+    ver = os.getenv('VERSION', 'undef')
     return "Hello World! Version: {}".format(ver)
 
 if __name__ == "__main__":
